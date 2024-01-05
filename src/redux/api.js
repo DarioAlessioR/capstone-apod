ff8397a8a5f3990eb8e6a3e203a0a56aea7c21d0
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2024-01-05 13:15:01 +0100

    Update api.js
    Change date generation logic to solve start of year bug

--- src/redux/api.js
+++ src/redux/api.js
@@ -4,18 +4,21 @@ const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
 const apiKey = 'OSCLzO1eeu2qJffkWDUEnhw5vgCZGBKQUF8gHUVJ';
 // previous = vFnwG1TUnH6EUE3InyLcQ3LeHeldl4GzaHH4TkGf
 
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth() + 0;
-const yyyy = today.getFullYear();
+let todayLessOneMonth = new Date() - 30 * 24 * 60 * 60 * 1000;
+todayLessOneMonth = new Date(todayLessOneMonth);
+let dd = todayLessOneMonth.getDate();
+let mm = todayLessOneMonth.getMonth() + 1;
+const yyyy = todayLessOneMonth.getFullYear();
+
 if (dd < 10) {
   dd = `0${dd}`;
 }
 if (mm < 10) {
   mm = `0${mm}`;
 }
-today = `${yyyy}-${mm}-${dd}`;
-const startdate = `&start_date=${today}&`;
+const startDate = `${yyyy}-${mm}-${dd}`;
+
+const startdate = `&start_date=${startDate}&`;
 const thumbs = '&thumbs=True&';
 
 const finalUrl = apiUrl + apiKey + startdate + thumbs;
